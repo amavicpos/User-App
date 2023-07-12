ce2ee6dce72cde28b8bf96c22eb8ae8032e03ba4
amavicpos <100805638+amavicpos@users.noreply.github.com>
2023-07-12 01:38:30 +0100

    Remove unused import

--- app/routes/users.js
+++ app/routes/users.js
@@ -2,7 +2,6 @@ const express = require('express');
 const fs = require('fs');
 
 const User = require('../models/User');
-const Image = require('../models/Image');
 
 const router = express.Router();
 
