b05b7c10de534fe3a6d76669132e7583176d4ec1
amavicpos <100805638+amavicpos@users.noreply.github.com>
2023-07-21 21:12:55 +0100

    Remove first user from main app file

--- app/app.js
+++ app/app.js
@@ -1,14 +1,9 @@
 const mongoose = require('mongoose');
 const bodyParse = require('body-parser');
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
 const ejsMate = require('ejs-mate');
 const app = express();
 // npm install mongoose body-parser express ejs fs multer path ejs-mate
-const User = require('./models/User');
-const Image = require('./models/Image');
-const Team = require('./models/Team');
 
 // ROUTE
 const UsersRouter = require('./routes/users');
@@ -28,57 +23,6 @@ mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log(`Mongodb Connected`))
     .catch(error => console.log(error));
-
-// FIRST USER
-// async function createInitialUser() {
-//     const initialImage1 = new Image({
-//         name: 'anna.jpg'
-//     });
-//     await initialImage1.save();
-    
-//     const initialTeam1 = new Team({
-//         name: 'Anna'
-//     });
-//     await initialTeam1.save();
-    
-//     const initialProfile1 = new User({
-//         name: 'Anna Smith',
-//         email: 'anna.smith@example.com',
-//         interests: 'coding',
-//         team: initialTeam1._id
-//     });
-//     await initialProfile1.save();
-
-//     await User.collection.drop();
-//     await Team.collection.drop();
-//     await Image.collection.drop();
-    
-//     const initialImage = new Image({
-//         name: 'anna.jpg',
-//         image: {
-//             srcUrl: `data:image/jpeg;base64,${Buffer.from(fs.readFileSync(path.join(__dirname, '\\images\\anna.jpg')).toString('base64'))}`
-//         }
-//     });
-//     await initialImage.save();
-    
-//     const initialTeam = new Team({
-//         name: 'Anna'
-//     });
-//     await initialTeam.save();
-    
-//     const initialProfile = new User({
-//         name: 'Anna Smith',
-//         email: 'anna.smith@example.com',
-//         interests: 'coding',
-//         team: initialTeam._id,
-//         picture: initialImage._id
-//     });
-//     await initialProfile.save();
-//     initialTeam.users.push(initialProfile);
-// }
-
-// createInitialUser();
-// console.log(initialTeam._id);
 
 app.use('/users', UsersRouter);
 app.use('/teams', TeamsRouter);
