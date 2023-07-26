ee21750161222ee592cfb450bab44f7e4a9532fd
amavicpos <100805638+amavicpos@users.noreply.github.com>
2023-07-26 20:10:42 +0100

    Add bar chart to statistics page

--- app/routes/statistics.js
+++ app/routes/statistics.js
@@ -9,9 +9,23 @@ router.get('/', async (req, res) => {
     try {
         const teams = await Team.find();
         const users = await User.find();
+        labelsChart = [];
+        if (Object.keys(teams).length > 0) {
+            teams.forEach(team => {
+                labelsChart.push(team.name);
+            });
+        }
+        dataChart = [];
+        if (Object.keys(teams).length > 0) {
+            teams.forEach(team => {
+                dataChart.push(team.users.length);
+            });
+        }
         res.render("statistics", {
             teams: (Object.keys(teams).length > 0 ? teams : {}),
-            users: (Object.keys(users).length > 0 ? users : {})
+            users: (Object.keys(users).length > 0 ? users : {}),
+            labelsChart: labelsChart,
+            dataChart: dataChart
         });
     } catch (error) {
         console.error('Error rendering view:', error);
