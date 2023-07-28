ae0ab4d4b004fd05ba9c640572390dc3eb0c388e
amavicpos <100805638+amavicpos@users.noreply.github.com>
2023-07-28 14:16:06 +0100

    Simple user guessing game

--- app/routes/game.js
+++ app/routes/game.js
@@ -1,13 +1,22 @@
 const express = require('express');
 
 const Team = require('../models/Team');
+const User = require('../models/User');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
         const teams = await Team.find();
-        res.render("game", {teams: (Object.keys(teams).length > 0 ? teams : {})});
+        const users = await User.find();
+        const user = users[Math.floor(Math.random() * users.length)];
+        const setQuestions = ['email', 'interests', 'team'];
+        const question = setQuestions[Math.floor(Math.random() * setQuestions.length)];
+        res.render("game", {
+            teams: (Object.keys(teams).length > 0 ? teams : {}),
+            user: user,
+            question: question
+        });
     } catch (error) {
         console.error('Error rendering view:', error);
         res.status(500).send('Error rendering view');
