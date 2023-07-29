94beea1c088b58d115b44fcbcc7e6658eaedfff0
amavicpos <100805638+amavicpos@users.noreply.github.com>
2023-07-29 18:40:02 +0100

    Add users icon start page

--- app/routes/start.js
+++ app/routes/start.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const Team = require('../models/Team');
 
@@ -7,7 +9,8 @@ const router = express.Router();
 router.get('/', async (req, res) => {
     const teams = await Team.find();
     res.render("start", {
-        teams: (Object.keys(teams).length > 0 ? teams : {})
+        teams: (Object.keys(teams).length > 0 ? teams : {}),
+        srcUrl: `data:image/png;base64,${Buffer.from(fs.readFileSync(path.join(__dirname, '..', 'public', 'images', 'userIcon.png')).toString('base64'))}`
     });
 });
 
